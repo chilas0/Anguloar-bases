062e901eed66faf82502a1f7b1d8590323ea37cf
chilas0 <chilas940@gmail.com>
2024-05-25 16:09:45 -0600

    first

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {  Component, Input } from '@angular/core';
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -10,9 +10,22 @@ import { Character } from '../../interfaces/character.interface';
 })
 export class ListComponent { 
 
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
   @Input()
   public characterList: Character[] = [{
+    id: 'd',
     name: 'Trunk',
     power: 10
   }];
+
+  
+
+  onDeleteCharacte( id?: string ):void {
+    //TODO: Emitir el ID del personaje
+    if( !id ) return;
+    this.onDelete.emit(id);
+    //console.log(this.characterList[index])
+  }
 }
